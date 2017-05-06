bb0f9ba9892f0d7c5c6221a9025dfeab3a9172f8
shuu01 <shuu01@gmail.com>
2017-05-06 19:43:08 +0300

    Create feeder.ino

---
+++ feeder.ino
@@ -0,0 +1,47 @@
+#include <Stepper.h>
+#include <TimeAlarms.h> // https://github.com/PaulStoffregen/TimeAlarms
+#include <RTClib.h> // https://github.com/adafruit/RTClib
+
+#define STEPS 32
+
+Stepper stepper(STEPS, 9, 8, 7, 6);
+RTC_DS1307 rtc;
+
+uint32_t sync_provider()
+{
+    /// sync provider for timelib sychronization
+    return rtc.now().unixtime();
+}
+
+void feed()
+{
+    stepper.step(1024);
+    digitalWrite(9, LOW);
+    digitalWrite(8, LOW);
+    digitalWrite(7, LOW);
+    digitalWrite(6, LOW);
+}
+
+void setup() 
+{
+    /// rtc setup
+    rtc.begin();
+    //rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    /// timelib setup
+    setSyncProvider(sync_provider);
+    setSyncInterval(86400); 
+    /// stepper setup
+    stepper.setSpeed(15);
+    /// timealarms
+    Alarm.alarmRepeat(6, 30, 0, feed);
+    Alarm.alarmRepeat(10, 30, 0, feed);
+    Alarm.alarmRepeat(14, 30, 0, feed);
+    Alarm.alarmRepeat(18, 30, 0, feed);
+    Alarm.alarmRepeat(23, 00, 0, feed);
+
+}
+
+void loop()
+{
+    Alarm.delay(0);
+}
